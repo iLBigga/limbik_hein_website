d0da594293bb5409248a074b4548828a378a33d0
iLBigga <Giannanti1996@gmail.com>
2023-01-04 19:36:05 +0100

    Add react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
+import { Link, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home"
+import { AboutMe } from "./pages/AboutMe"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about-me">About Me</Link></li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={ <Home />}/>
+      <Route path="/about-me" element={ <AboutMe />} />
+    </Routes>
+    </>
   );
 }
 
---
+++ src/pages/AboutMe.js
@@ -0,0 +1,3 @@
+export function AboutMe() {
+    return <h1>About me</h1>
+}
