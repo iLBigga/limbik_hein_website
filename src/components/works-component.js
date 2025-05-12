b6ddc5dd62e45b40921b5e237de8f658bf646d16
shred66 <91328968+shred66@users.noreply.github.com>
2025-05-12 18:34:57 +0200

    Update works-component.js
    prova 1 cambio di griglia di visualizzazione

--- src/components/works-component.js
+++ src/components/works-component.js
@@ -12,19 +12,17 @@ export default function WorksComponent(props) {
             <h1 className="text-xl pb-5">{props.title}</h1>
           </div>
         </div>
-        <div className="flex px-2 md:grid grid-cols-3 md:gap-5 md:grid-cols-12">
-          <div className="col-span-1 md:col-span-2"></div>
-          <div className="col-span-2 md:col-span-6">
-            {props.works.map((workList, index) => (
+        <div className="px-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {props.works.map((workList, index) => (
+            <div key={index}>
               <WorkComponent 
-                key={index}
                 imageSrc={workList.image} 
                 title={workList.title} 
-                date={moment(workList.date*1000).format('L')} 
-                description={workList.description}>
-              </WorkComponent>
-            ))}
-          </div>
+                date={moment(workList.date * 1000).format('L')} 
+                description={workList.description}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </section>
